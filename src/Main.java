cad9e61ccadb91cb85ff27b9d0e813506703cab2
Lara <81490646+larasouto@users.noreply.github.com>
2022-08-04 14:52:28 -0300

    Update Main

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,9 @@ import mapa.HashMapArvore;
 import java.lang.*;
 
 public class Main {
+}
+class MainVetor {
     public static void main(String[] args) {
-
         int count = 100000;
         Veiculo[] veiculos = new Veiculo[count];
         for (int i = 0; i < count; i++) {
@@ -38,19 +39,26 @@ public class Main {
         long tempoI4 = System.nanoTime();
         mapaV.removeMenorIgual();
         long tempoF4 = System.nanoTime();
-        // String printMenorIgual = mapa.print();
-        // System.out.println(printMenorIgual);
 
-        System.out.println("Tempo total operações VETOR");
-        System.out.println("Inserção de 100 mil veículos = " + (tempoF - tempoI));
-        System.out.println("Verificação do número de veículos da marca Ford = " + (tempoF2 - tempoI2));
-        System.out.println("Apresentação dos veículos = " + (tempoF3 - tempoI3));
-        System.out.println("Remoção dos veículos com Chassi igual ou inferior à 202050000 = " + (tempoF4 - tempoI4));
+        System.out.println("Tempo total operacoes VETOR");
+        System.out.println("Insercao de 100 mil veiculos = " + (tempoF - tempoI));
+        System.out.println("Verificacao do numero de veiculos da marca Ford = " + (tempoF2 - tempoI2));
+        System.out.println("Apresentacao dos veiculos = " + (tempoF3 - tempoI3));
+        System.out.println("Remocao dos veiculos com Chassi igual ou inferior a 202050000 = " + (tempoF4 - tempoI4));
+    }
+}
+class MainLDE {
+    public static void main(String[] args) {
+        int count = 100000;
+        Veiculo[] veiculos = new Veiculo[count];
 
-        int count2 = 100000;
-        HashMapLDE mapaLDE = new HashMapLDE(count2);
+        for (int i = 0; i < count; i++) {
+            veiculos[i] = new Veiculo();
+        }
+
+        HashMapLDE mapaLDE = new HashMapLDE(count);
         long tempoI5 = System.nanoTime();
-        for (int i = 0; i < count2; i++) {
+        for (int i = 0; i < count; i++) {
             mapaLDE.put(veiculos[i].getChassi(), veiculos[i]);
         }
         long tempoF5 = System.nanoTime();
@@ -67,14 +75,18 @@ public class Main {
         long tempoI8 = System.nanoTime();
         mapaLDE.removeMenorIgualLDE();
         long tempoF8 = System.nanoTime();
-        // String printMenorIgual = mapaLDE.print();
-        // System.out.println(printMenorIgual);
 
         System.out.println("Tempo total operações LDE");
         System.out.println("Inserção de 100 mil veículos = " + (tempoF5 - tempoI5));
         System.out.println("Verificação do número de veículos da marca Ford = " + (tempoF6 - tempoI6));
         System.out.println("Apresentação dos veículos = " + (tempoF7 - tempoI7));
         System.out.println("Remoção dos veículos com Chassi igual ou inferior à 202050000 = " + (tempoF8 - tempoI8));
+    }
+}
+class MainABB {
+    public static void main(String[] args) {
+        int count = 100000;
+        Veiculo[] veiculos = new Veiculo[count];
 
         ABB abb = new ABB();
         long tempoI9 = System.nanoTime();
@@ -98,6 +110,6 @@ public class Main {
         System.out.println("Verificação do número de veículos da marca Ford = " + (tempoF10 - tempoI10));
         System.out.println("Apresentação dos veículos = " + (tempoF11 - tempoI11));
         System.out.println("Remoção dos veículos com Chassi igual ou inferior à 202050000 = " + (tempoF12 - tempoI12));
-
     }
 }
+
